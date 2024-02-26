4e3bcc45ba756431af52fcd8550361ece127bf69
Alexey Shvechkov <alexey@shvechkov.com>
2024-02-25 22:21:20 -0500

    week 7

---
+++ ex6-octave/dataset3Params.m
@@ -0,0 +1,69 @@
+function [C, sigma] = dataset3Params(X, y, Xval, yval)
+%DATASET3PARAMS returns your choice of C and sigma for Part 3 of the exercise
+%where you select the optimal (C, sigma) learning parameters to use for SVM
+%with RBF kernel
+%   [C, sigma] = DATASET3PARAMS(X, y, Xval, yval) returns your choice of C and 
+%   sigma. You should complete this function to return the optimal C and 
+%   sigma based on a cross-validation set.
+%
+
+% You need to return the following variables correctly.
+C = 1;
+sigma = 0.3;
+
+% ====================== YOUR CODE HERE ======================
+% Instructions: Fill in this function to return the optimal C and sigma
+%               learning parameters found using the cross validation set.
+%               You can use svmPredict to predict the labels on the cross
+%               validation set. For example, 
+%                   predictions = svmPredict(model, Xval);
+%               will return the predictions on the cross validation set.
+%
+%  Note: You can compute the prediction error using 
+%        mean(double(predictions ~= yval))
+%
+
+
+% Train the SVM
+
+vals=[0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30]
+
+C_min = 1;
+sigma_min = 0.3;
+pmin = 10000000000000000000000000;
+
+for i = 1:length(vals)
+    C = vals(i);
+    for j = 1:length(vals)
+        sigma = vals(j);
+
+        % disp(["C=" num2str(C) " sigma=" num2str(sigma) ])
+        model= svmTrain(X, y, C, @(x1, x2) gaussianKernel(x1, x2, sigma));
+
+        predictions = svmPredict(model, Xval);        
+        pm = mean(double(predictions ~= yval));
+        % disp(["pm=" num2str(pm) " pmin=" num2str(pmin) ])
+
+        if (pm < pmin)            
+            pmin = pm;
+            C_min = C;
+            sigma_min = sigma;
+        end
+        
+
+    end
+end
+
+C = C_min;
+sigma = sigma_min;
+
+% disp(["Optimal C=" num2str(C) " sigma=" num2str(sigma) ])
+
+
+
+
+
+
+% =========================================================================
+
+end
